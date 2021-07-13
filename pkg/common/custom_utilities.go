f455abc5836f432ee20d061c143f1d9287aea5d7
Ubuntu <ubuntu@ip-172-31-14-101.us-east-2.compute.internal>
2021-07-13 12:05:56 +0000

    Included files from elastic cache for unit testing. Replaced elasticache sample code with sagemaker equivalents. Made a demo sage maker test for all resources detecting and setting terminal on InvalidParameterValue. Added unit testing coverage, and using default values in demo test files. Removed -f from clean-mocks command in Makefile. Moved custom_update_api_test.go to manager_test_suite_test.go. Added startScenio function to test_suite_runner.go for panic catching, and added similar panic catching technique in manager_test_suite_test.go. Changed role ARN to dummy value in cr testing yaml files, and renamed the featureGroupName in the invalid_create_attempt. Removed local-test from Makefile. Added yamlequal call to test_suite_runner.go. Added transform job unit testing demo, and fixed bug that was revealed in custom_delta.go from unit testing. Updated test suite names and empty mock api functions.

--- pkg/common/custom_utilities.go
+++ pkg/common/custom_utilities.go
@@ -13,6 +13,17 @@
 
 package common
 
+import (
+       	"fmt"
+	"os"
+	"io/ioutil"
+	"os/exec"
+)
+
+var	TestDataDirectory = "testdata"
+var	DefaultTimestamp = "0001-01-01T00:00:00Z"
+var	ReplaceTimestampRegExp = "s/\"[0-9]{4}-[0-9]{2}-[0-9]{2}T[0-9]{2}:[0-9]{2}:[0-9]{2}Z\"/\"" + DefaultTimestamp + "\"/"
+
 // IsModifyingStatus returns true if a
 // resource's latest status matches one
 // of the provided modifying statuses.
@@ -26,4 +37,71 @@ func IsModifyingStatus(
 		}
 	}
 	return false
+}
+
+// Checks to see if the contents of a given yaml file, with name stored
+// in expectation, matches the given actualYamlByteArray.
+func IsYamlEqual(expectation *string, actualYamlByteArray *[]byte) bool {
+     	// Get the file name of the expected yaml.
+	expectedYamlFileName := TestDataDirectory + "/" + *expectation
+
+	// Build a tmp file for the actual yaml.
+	actualYamlFileName := buildTmpFile("actualYaml", *actualYamlByteArray)
+	defer os.Remove(actualYamlFileName)
+	if "" == actualYamlFileName {
+	  fmt.Printf("Could not create temporary actual file.\n")
+	  return false
+	}
+
+	// Replace Timestamps that would show up as different.
+	_, err := exec.Command("sed", "-r", "-i", ReplaceTimestampRegExp, actualYamlFileName).Output()
+	if !checkExecCommandError(err) {
+	    return false
+	}
+	
+	output,err := exec.Command("diff", "-c", expectedYamlFileName, actualYamlFileName).Output()
+	if !checkExecCommandError(err) {
+	   return false
+	}
+
+	if len(output) > 0 {
+	   actualOutput,_ := exec.Command("cat", actualYamlFileName).Output()
+	   fmt.Printf("\nActual Yaml File Instead of: " + expectedYamlFileName + "\n" + string(actualOutput) + "\n")
+	   fmt.Printf("Diff From Expected:\n" + string(output) + "\n")
+	   return false
+	}
+	return true
+}
+
+func buildTmpFile(fileNameBase string, contents []byte) string {
+     newTmpFile, err := ioutil.TempFile(TestDataDirectory, fileNameBase)
+     if err != nil {
+        fmt.Println(err)
+	return ""
+     }
+     if _, err := newTmpFile.Write(contents); err != nil {
+        fmt.Println(err)
+	return ""
+     }
+     if err := newTmpFile.Close(); err != nil {
+        fmt.Println(err)
+	return ""
+     }		
+     return newTmpFile.Name()
+}
+
+func checkExecCommandError(err error) bool {
+     if err == nil {
+     	return true
+     }
+     switch err.(type) {
+	case *exec.ExitError:
+	       // ExitError is expected.
+	       return true
+	default:
+	       // Couldn't run diff.
+	       fmt.Printf("Exec Command Error: ")
+	       fmt.Println(err)
+	       return false
+     }
 }
